70a40c28abbabe90336082f1bbc0214546bd32e2
Guilolo10 <guilherme.souza802@etec.sp.gov.br>
2025-06-16 18:49:28 -0300

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -24,16 +24,35 @@ do{
 	else if (operaçao == '-'){
 		resultado = subtrair(num1,num2);
 		printf("resultado:%.2f\n", resultado);
-
-	}
+}
 
 	else if (operaçao == '*') {
 	     resultado = multipicar(num1,num2);
-	     printf("resultado:%.2f\n");
+	     printf("resultado:%.2f\n", resultado);
+}  
+
+        else if (operaçao == '/') {
+            if (num2 != 0) {
+	       resultado = dividir(num1,num2);
+	        printf("resultado%.2f\n", resultado);
+	    } else { 
+		    printf("divisao por 0 não permitida\n");
+	    }
+	} else { 
+		printf("operaçao invalida\n");
+	}
+
+            printf("Desejar fazer outro calculo? (Digite 'q' para sair ou qualquer outra tecla para continuar):");
+             scanf(" %c", &escolha);
+} while (escolha != 'q');
+  printf("Programa encerrado\n");
+
+return 0;
+
 
 
 	
-
+}
 	
 
 }
