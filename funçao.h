5c49a5036cb88bf1b797a8a3ec8c6ae8de00d45d
Guilolo10 <guilherme.souza802@etec.sp.gov.br>
2025-06-16 19:01:50 -0300

    Create funçao.h

---
+++ funçao.h
@@ -0,0 +1,9 @@
+#ifndef FUNCAO_H
+#define FUNCAO_H
+
+float somar(float a, float b);
+float subtrair(float a, float b);
+float multiplicar(float a, float b);
+float dividir(float a, float b);
+
+#endif
